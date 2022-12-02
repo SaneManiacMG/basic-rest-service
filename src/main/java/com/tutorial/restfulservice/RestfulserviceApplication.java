73e7d277797780cb5374651ad464c322d1750660
SaneManiacMG <mmoroane@hotmail.com>
2022-12-02 15:44:42 +0200

    Added @RestController annotation to main class

--- src/main/java/com/tutorial/restfulservice/RestfulserviceApplication.java
+++ src/main/java/com/tutorial/restfulservice/RestfulserviceApplication.java
@@ -3,8 +3,10 @@ package com.tutorial.restfulservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestfulserviceApplication {
 
 	public static void main(String[] args) {
