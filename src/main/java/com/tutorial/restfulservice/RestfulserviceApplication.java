8057a46554f46d50bc8712e839930fdb737423bc
Bathande95 <110979702+Bathande95@users.noreply.github.com>
2022-12-02 13:37:39 +0200

    Init commit

---
+++ src/main/java/com/tutorial/restfulservice/RestfulserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulserviceApplication.class, args);
+	}
+
+}
